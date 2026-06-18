53f6f3dda9ed794a5126db3f3e48398cf2e718d6
H. Boateng <haboateng@users.noreply.github.com>
2026-06-18 08:36:13 -0700

    updating kmeans_feasible_set
    Added a new algorithm and updated required inputs

--- codes/kmeans_feasible_set.m
+++ codes/kmeans_feasible_set.m
@@ -1,21 +1,30 @@
-function sets = kmeans_feasible_set(M,snum,kdim)
+function sets = kmeans_feasible_set(M,score,snum,kdim,pickalg)
 % k-means clustering for selecting a subset of chips from a given set
 % Input
 % M - matrix of faithful from Walsh Hadamard matrix
 % score - a vector with the Walsh score of each row of M
 % snum - cardinality of set (i.e. number of clusters => k=snum
 % kdim - dimension of embedding
+% pickalg - flag for which algorithm to pick
 % Output
-% sets - 
+% sets - A set of the best snum indices.
 
-[~, rsize]=size(M); % number of rows of the matrix
-if kdim > rsize
-    disp('size of lower-dimension has to be less than the length of a row of the input matrix')
-    return;
+[csize,rsize]=size(M);
+if length(score) ~= csize
+    disp('length of score must be the same as that column length of input matrix')
+    return
 end
-
+if pickalg == 1
+    M = [M reshape(score,csize,1)];
+else
+    if kdim > rsize
+        disp('size of lower-dimension has to be less than the length of a row of the input matrix')
+        return;
+    end
+    
+    M = M*randn(rsize,kdim)/sqrt(kdim); % "lower-dimensional" Gaussian embedding
+end
 lind=(1:1:rsize)';
-M = M*randn(rsize,kdim)/sqrt(kdim); % "lower-dimensional" Gaussian embedding
 %opts=statset('Display','final');
 % Define the number of clusters and perform k-means clustering
 %[idx, centroids] = kmeans(M, snum,Distance="sqeuclidean",Replicates=kdim,Options=opts);
