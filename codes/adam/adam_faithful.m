87ff8598f6d22527ed39673aa953cc960d75ea7a
louispadam <adamnp.lp3@gmail.com>
2026-07-03 10:19:08 -0400

    Updated figures and codes
    Refined some of the figures and used updated codebook algorithms

--- codes/adam/adam_faithful.m
+++ codes/adam/adam_faithful.m
@@ -1,29 +1,34 @@
 function return_data = adam_faithful(inp_seq,cutoff)
+% ADAM_FAITHFUL applies a low-pass filter characterized by some given
+% cutoff to a given input sequence, and returns the result
+%
+%last updated 06/26/2026 by Adam Petrucci
+arguments
+    inp_seq       % chip sequence to process
+    cutoff        % truncation value for low-pass filter
+end
 
+    % Construct coarse-mesh difference representation of input sequence in
+    % physical (as opposed to discrete) space. cumsum is built-in Matlab
+    % function for constructing cumulative sum of vector
     S = [0; cumsum(inp_seq)];
 
+    % Set up FFT
     M = length(S);
     k = [0:floor(M/2)-1 -ceil(M/2):-1]';
     xi = 2*pi*k;
 
+    % Apply low-pass filter in frequency space using built-in Matlab
+    % functionality for Fourier analysis
     Fhat = fft(S);
     mask = abs(xi) <= cutoff;
     f_filt = real(ifft(Fhat .* mask));
 
+    % Interpret output in physical space as chip-sequence. diff computes
+    % the difference between subsequent values
     seq_new = 2*(diff(f_filt) >= 0) - 1;
 
-    %one = figure(1);
-    %clf(one);
-    %one_ax = axes(one);
-    %plot(one_ax,1:length(S),S);
-    %title(one_ax,'Input');
-
-    %two = figure(2);
-    %clf(two);
-    %two_ax = axes(two);
-    %plot(two_ax,1:length(f_filt),f_filt);
-    %title(two_ax,'Output');
-
+    % Return data
     return_data = seq_new;
 
 end
--- codes/adam/construct_lawbook.m
+++ codes/adam/construct_lawbook.m
@@ -1,9 +1,28 @@
 function return_data = construct_lawbook(w,faithful,find_best)
+% CONSTRUCT_LAWBOOK greedily selects a maximal set of codebooks from a set
+% of faithful sequences using a given selection algorithm. 'Maximal' in
+% this setting means as many distinct (disjoint) codebooks as can be
+% constructed from the given faithful set with the constraint on codebook
+% size
+%
+%last updated 06/26/2026 by Adam Petrucci
+arguments
+    w           % length of word (ie bits/word)
+    faithful    % set of faithful rows
+    find_best   % algorithm for constructing codebook; algorithm must take
+                % two inputs: the set of faithful rows and the word size,
+                % and must return one output: a vector of walsh frequencies
+end
 
+    % Maximum number of codebooks of size 2^w with given faithful set
+    % (cds = codebooks)
     cds = floor(length(faithful)/(2^w));
 
+    % Initialize storage objects
     lawbook = zeros(cds,2^w);
 
+    % Greedily collect codebooks by iteratively finding best codebook then
+    % removing those used sequences from the admissible set
     for cd = 1:cds
 
         best = find_best(faithful,w);
@@ -12,6 +31,7 @@ function return_data = construct_lawbook(w,faithful,find_best)
 
     end
 
+    % Return data
     return_data = lawbook;
 
 end
